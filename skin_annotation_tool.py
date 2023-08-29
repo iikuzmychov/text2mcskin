51c2e57ddfc58a15921949ec080e7d5eadab5262
Ihor Kuzmychov <iikuzmychov@gmail.com>
2023-08-29 23:38:11 +0200

    Create annotation tool

---
+++ skin_annotation_tool.py
@@ -0,0 +1,149 @@
+import csv
+import os
+import PySimpleGUI as gui
+import asyncio
+import numpy as np
+from typing import Optional
+from minepi import Skin
+from PIL import Image, ImageOps
+from io import BytesIO
+from scipy.ndimage import binary_dilation
+from constants import SKINS_DIRECTORY, CSV_FILENAME, CSV_DELIMITER
+from pathlib import Path
+
+SKIN_RENDER_HORIZONTAL_ROTATION = 35
+
+
+def outline_image(img: Image, outline_color=(255, 255, 255)) -> Image:
+    data = np.array(img)
+    alpha_channel = data[..., 3] > 128
+    outline = binary_dilation(alpha_channel) & ~alpha_channel
+    data[outline, :3] = outline_color
+    data[outline, 3] = 255
+
+    return Image.fromarray(data)
+
+
+async def render_skin(filename: str, hr: int, draw_outline: bool) -> bytes:
+    skin = Skin(raw_skin=Image.open(f"{SKINS_DIRECTORY}/{filename}"))
+
+    render = await skin.render_skin(hr=hr, vr=-20, vrra=-5, vrla=5, vrrl=-5, vrll=5, aa=True)
+
+    if draw_outline:
+        render = ImageOps.expand(render, border=1, fill=(255, 255, 255, 0))
+        render = outline_image(render)
+
+    with BytesIO() as bio:
+        render.save(bio, format="PNG")
+        return bio.getvalue()
+
+
+def init_csv_file():
+    if not os.path.exists(CSV_FILENAME):
+        with open(CSV_FILENAME, 'w', newline='', encoding='utf-8') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=CSV_DELIMITER)
+            csv_writer.writerow(['filename', 'description'])
+
+
+def read_description(filename: str) -> Optional[str]:
+    with open(CSV_FILENAME, 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=CSV_DELIMITER)
+        for row in csv_reader:
+            if row[0] == filename:
+                return row[1]
+
+    return None
+
+
+def update_description(filename: str, description: str):
+    skins = []
+    updated = False
+    with open(CSV_FILENAME, 'r', encoding='utf-8') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=CSV_DELIMITER)
+        for row in csv_reader:
+            if row[0] == filename:
+                row[1] = description
+                updated = True
+            skins.append(row)
+
+    if not updated:
+        skins.append([filename, description])
+
+    with open(CSV_FILENAME, 'w', newline='', encoding='utf-8') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=CSV_DELIMITER)
+        csv_writer.writerows(skins)
+
+
+class MainWindow:
+    def __init__(self) -> None:
+        self.__init_window()
+
+        self.__skins = sorted(os.listdir(SKINS_DIRECTORY), key=lambda filename: int(Path(filename).stem))
+        self.draw_skin_outline = True
+        self.current_skin_index = 0
+
+    @property
+    def current_skin_index(self) -> int:
+        return self.__current_skin_index
+
+    @current_skin_index.setter
+    def current_skin_index(self, value) -> None:
+        self.__current_skin_index = value
+        self.__current_description = read_description(self.current_skin)
+        self.__load_skin()
+
+    @property
+    def current_skin(self) -> str:
+        return self.__skins[self.current_skin_index]
+
+    def run(self) -> None:
+        while True:
+            event, values = self.__window.read()
+
+            if event == gui.WINDOW_CLOSED:
+                break
+            elif event == "-NEXT-SKIN-BUTTON-":
+                self.current_skin_index = (self.current_skin_index + 1) % len(self.__skins)
+            elif event == "-PREVIOUS-SKIN-BUTTON-":
+                self.current_skin_index = (self.current_skin_index - 1) % len(self.__skins)
+            elif event == "-FIRST-SKIN-BUTTON-":
+                self.current_skin_index = 0
+            elif event == "-LAST-SKIN-BUTTON-":
+                self.current_skin_index = len(self.__skins) - 1
+            elif event == "-SAVE-DESCRIPTION-BUTTON-":
+                update_description(self.current_skin, values["-SKIN-DESCRIPTION-INPUT-"])
+            elif event == "-SKIN-DESCRIPTION-INPUT-":
+                self.__on_skin_description_input_changed(values)
+            elif event == "-TOGGLE-SKIN-OUTLINE-BUTTON-":
+                self.draw_skin_outline = not self.draw_skin_outline
+                self.__load_skin()
+
+        self.__window.close()
+
+    def __init_window(self) -> None:
+        layout = [
+            [gui.Button("Toggle outline", key="-TOGGLE-SKIN-OUTLINE-BUTTON-")],
+            [gui.Text("Front view"), gui.Text(size=(15, 1)), gui.Text("Back view")],
+            [gui.Image(key="-SKIN-FRONT-IMAGE-", size=(220, 420)), gui.Image(key="-SKIN-BACK-IMAGE-", size=(220, 420))],
+            [gui.Button("<<<", key="-FIRST-SKIN-BUTTON-"), gui.Button("<", key="-PREVIOUS-SKIN-BUTTON-", size=(4, None)), gui.Text(key="-SKIN-FILENAME-TEXT-", size=(15, ), justification="center"), gui.Button(">", key="-NEXT-SKIN-BUTTON-", size=(4, None)), gui.Button(">>>", key="-LAST-SKIN-BUTTON-")],
+            [gui.Multiline(key='-SKIN-DESCRIPTION-INPUT-', size=(60, 4), expand_x=True, no_scrollbar=True, enable_events=True)],
+            [gui.Button("Save", key="-SAVE-DESCRIPTION-BUTTON-", expand_x=True, disabled=True)]
+        ]
+        self.__window = gui.Window("Skin Annotator Tool", layout, element_justification="center", finalize=True)
+
+    def __on_skin_description_input_changed(self, values: dict):
+        save_button_disabled = values["-SKIN-DESCRIPTION-INPUT-"].isspace() or (values["-SKIN-DESCRIPTION-INPUT-"] == self.__current_description)
+        self.__window["-SAVE-DESCRIPTION-BUTTON-"].update(disabled=save_button_disabled)
+
+    def __load_skin(self) -> None:
+        self.__window["-SKIN-FRONT-IMAGE-"].update(data=asyncio.run(render_skin(self.current_skin, SKIN_RENDER_HORIZONTAL_ROTATION, self.draw_skin_outline)), size=(220, 420))
+        self.__window["-SKIN-BACK-IMAGE-"].update(data=asyncio.run(render_skin(self.current_skin, SKIN_RENDER_HORIZONTAL_ROTATION - 180, self.draw_skin_outline)), size=(220, 420))
+        self.__window["-SKIN-FILENAME-TEXT-"].update(self.current_skin)
+        self.__window["-SKIN-DESCRIPTION-INPUT-"].update(self.__current_description or "")
+
+
+if __name__ == "__main__":
+    init_csv_file()
+
+    gui.theme("Black")
+    MainWindow().run()
